994e0ee3d5d47acd706b92d23e3bbdccccd4615c
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2026-04-01 21:58:39 +0300

    Variable,Strings,Loops

--- intro/src/main/java/com/turkcell/Main.java
+++ intro/src/main/java/com/turkcell/Main.java
@@ -1,8 +1,154 @@
 package com.turkcell;
 
-//Entrypoint
+import java.util.Arrays;
+
+// Entrypoint
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Turkcell!");
+        System.out.println("Merhaba Turkcell, Java'ya hoş geldiniz!");
+
+        // Programlama konseptleri
+
+        // Scope Kavramı => {} kapsama alanı
+
+        // Değişkenler (Variables)
+        // Kodun akışında değer tutan isimli veriler.
+
+        System.out.println(10);
+        int X = 15; // Değişken tanımlandı. X ismine bir değer atandı.
+        // Tanımlandıktan itibaren değişebilir, erişilebilir.
+        System.out.println(X);
+        X = 20;
+        System.out.println(X);
+
+        // Değişken tipleri => int, double, boolean, char, String
+        String name = "Halit";
+        String age = "25";
+        boolean isStudent = true;
+        char grade = 'A';
+
+        // Diziler (Arrays)
+
+        String[] names = { "Halit", "Ayşe", "Mehmet" };
+        System.out.println(names[0]); // index, 0 dan başlar.
+        // System.out.println(names[5]);
+
+        // Primitive (ilkel) tipler -> int, double,boolean,char
+        int a = 0;
+        int b = a;
+        a = 10;
+        System.out.println(a);
+        System.out.println(b);
+
+        // Referans tipler -> String, Array, Object
+        int[] c = { 0, 1, 2, 3 };
+        int[] d = c;
+        d[3] = 30;
+        System.out.println(c[3]);
+        System.out.println(d[3]);
+
+        System.out.println("******");
+
+        System.out.println(a == b);
+        System.out.println(c == d); /* int, double değeri kopyalar array, object adresi kopyalar */
+
+        int[] x = { 0, 1, 2, 3 };
+        int[] y = { 0, 1, 2, 3 };
+        System.out.println(y);
+        System.out.println(x == y);
+        System.out.println(Arrays.equals(x, y));
+
+        String s1 = "Merhaba";
+        String s2 = "Merhaba";
+        System.out.println(s1 == s2); // String Pool : Aynı değer olduğu için aynı adresi gösterir
+        /*
+         * Java, aynı stringleri tekrar tekrar oluşturmamak için
+         * String Pool (havuz) kullanır.
+         * 
+         * Aynı değer → aynı referans (adres)
+         */
+        // (Aynı metinlerin bir havuzda toplanıp performans için birebir olanları aynı
+        // referansa ata.) Değerleri aynı olan string değişkenlere aynı adresi veriyor.
+
+        // Yine de daha güvenli bir karşılaştırma için equals() kullanılır.equals():
+        // değer karşılaştırır (her zaman güvenli)
+        System.out.println(s1.equals(s2));
+
+        String s3 = "Turkcell";
+        String s4 = s3.intern();
+        // intern() metodu, s3'ün değerini String Pool'a ekler ve oradaki referansı
+        // döndürür.
+
+        System.out.println(s3 == s4);
+
+        String str3 = "Turkcell";
+        String str4 = new String("Turkcell"); // instance oluşturur, farklı referans
+
+        System.out.println(str3 == str4); // false, farklı referanslar
+
+        /*
+         * string immutable demiştik ama
+         * String name = "Halit";
+         * System.out.println(name);
+         * name = "25";
+         * System.out.println(name); burada farklı yazıyor “
+         * 
+         * String immutable’dır; değişiklik yapıldığında mevcut nesne değişmez, yeni bir
+         * nesne oluşturulup referans ona yönlendirilir.”
+         * String değişmiyor ama eski hali nerde tutuluyor?”Heap memory içinde,
+         * özellikle String Pool’da tutuluyor.
+         * Eski string silinmez, hafızada durur .Hiçbir değişken onu göstermiyorsa
+         * Garbage Collector (çöp toplayıcı) gelip temizle
+         */
+        System.out.println("Merhaba" + " " + "Dünya!");
+
+        System.out.println("**********");
+
+        System.out.println(10 * 20);
+        System.out.println(10 / 3);
+
+        System.out.println(1 == 1);
+        System.out.println(1 != 1);
+        System.out.println(5 > 10);
+
+        a = a + 5;
+        a += 5;
+        a++; // a = a + 1;
+        System.out.println("**********");
+        // Döngüler
+
+        // X işlemini birden fazla kez çalıştırmak.
+        // iteration = yineleme
+        // değişken, koşul, her iterasyon sonrası işlem
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        String[] students = { "Halit", "Ayşe", "Mehmet" };
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+        for (String z : students) { // students dizisindeki her elemanı sırayla al → String z’ye koy → çalıştır
+            System.out.println(z);
+        }
+
+        // iterasyon => koşul
+        int whileDongusu = 0;
+        while (whileDongusu < 5) {
+            System.out.println("Sonsuz döngü");
+            whileDongusu++;
+        }
+
+        String name2 = "Halit";
+        System.out.println(name2);
+        name2 = "Nurgül";
+        System.out.println(name2);
+        String name3 = name2.concat("abc");
+        // String immutable (değiştirilemez) bir yapıya sahiptir.
+        // concat() gibi metotlar yeni bir String oluşturur, mevcut String'i
+        // değiştirmez.
+        System.out.println(name3);
+
     }
-}
+
+} // Main classının kapsama alanı (sınır)

2d0d5edee84e47f4c28ab5f46ef07377e3c2d8e9
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2026-04-14 22:12:42 +0300

    Add product service implementation and update controller

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -1,87 +1,33 @@
 package com.turkcell.spring_starter.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.turkcell.spring_starter.dto.ProductCreatedResponse;
 import com.turkcell.spring_starter.dto.ProductForCreateDto;
-import com.turkcell.spring_starter.model.Product;
-import org.springframework.web.bind.annotation.PutMapping;
+import com.turkcell.spring_starter.service.ProductServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 // Altın kural: Veritabanı nesneleri requestte de responseda da kullanılamaz.
-@RestController
+@RestController // Uygulamada gerektiğinde controlleri newleyebilmek için spring'e haber
+                // veririz. Spring, controlleri newler ve onun üzerinden istekleri karşılar.
 @RequestMapping("/api/product")
 public class ProductController {
-    // InMemory çalış(değişkenlerle çünkü değişkenler ramde tutulur.)
-    private List<Product> productList = new ArrayList<>();
 
-    @GetMapping()
-    public List<Product> getAllProducts() {
-        return productList; // Veritabanındaki Product nesnelerini listele.
+    // private final ProductServiceImpl prouctServiceImple = new ProductServiceImpl
+    // YANLIŞ
+
+    private final ProductServiceImpl productServiceImpl; // Bağımlılık Enjeksiyonu -> Dependency Injection
+
+    public ProductController(ProductServiceImpl productServiceImpl) {
+        this.productServiceImpl = productServiceImpl;
     }
 
-    @GetMapping("/{id}")
-    public Product getProductById(@PathVariable int id) { // detay sayfası için id'ye göre ürün getirmirirz daha
-                                                          // detaylıdır burası
-        return productList.stream().filter(i -> i.getId() == id).findFirst().orElse(null);
+    @PostMapping("path")
+    public ProductCreatedResponse create(@RequestBody ProductForCreateDto productForCreateDto) {
+
+        return this.productServiceImpl.create(productForCreateDto);
     }
 
-    // Request-Response Pattern =>
-    // Her istek cevap kendine ait bir modele sahip olmak zorunda.Birebir başka bir
-    // istek-cevap çiftiyle aynı içeriğe sahip olabilirler ama birbirlerinin aynısı
-    // olamazlar.
-    @PostMapping
-    public ProductCreatedResponse createProduct(@RequestBody ProductForCreateDto productDto) {
-        // Sen dışarodan ProductForCreateDto alıyorsun ama senin veritabanında Product
-        // nesnesi var. O yüzden ProductForCreateDto'yu Product'a çevirmen gerekiyor.
-        if (productDto.getPrice() < 0)
-            throw new RuntimeException("Para 0'dan küçük olamaz.");
-
-        // Transfer -> Manuel Mappıng
-        Product product = new Product();
-        product.setName(productDto.getName());
-        product.setPrice(productDto.getPrice());
-        product.setId(productList.size() + 1); // Id'yi kendimiz atıyoruz çünkü veritabanı yok.
-
-        productList.add(product);
-        // Veritabanına product nesnesini ekle..
-
-        // ProductCreatedResponse => Dışarıya dönmek istediğimiz cevaba ait model.
-        // Entity Nesnesi -> Dto
-        ProductCreatedResponse response = new ProductCreatedResponse();
-        response.setId(product.getId());
-        response.setName(product.getName());
-        response.setPrice(product.getPrice());
-
-        return response; // Dışarıya ProductCreatedResponse dönüyoruz çünkü biz dışarıya Product
-                         // nesnesini
-        // dönmek istemiyoruz.Çünkü Product nesnesi veritabanı nesnesidir ve biz
-        // veritabanı nesnelerini dışarıya dönmek istemiyoruz.
-
-        // Ben controller olarak iş kodu çalıştıramam, ama bunu yapmam gerekli..
-        // İş kodunu çalıştıracak olan yapıya BAĞIMLIYIM.
-        // Bağımlılık Enjeksiyonu -> Dependency Injection
-
-    }
-
-    @PutMapping
-    public void updateProduct(@RequestBody Product product) {
-        Product productToUpdate = productList.stream().filter(i -> i.getId() == product.getId()).findFirst()
-                .orElseThrow(() -> new RuntimeException("Product not found"));
-        productToUpdate.setName(product.getName());
-        productToUpdate.setPrice(product.getPrice());
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable int id) {
-        productList.removeIf(i -> i.getId() == id); // Ürünü veritabanından siler
-    }
 }
--- spring-starter/src/main/java/com/turkcell/spring_starter/service/ProductServiceImpl.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/service/ProductServiceImpl.java
@@ -1,12 +1,47 @@
 package com.turkcell.spring_starter.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+import com.turkcell.spring_starter.dto.ProductCreatedResponse;
+import com.turkcell.spring_starter.dto.ProductForCreateDto;
+import com.turkcell.spring_starter.model.Product;
+
 //Service => İş katmanını temsil eder. Veritabanı işlemleri, iş kuralları gibi işlemler burada yapılır.
 //Implementation 
 //ProductService => Interface
 // ProductServiceImpl => Class
+
+@Service // IOC ye bu tütü eklediniğinde bu classı newleyebilirsin demektir. Spring, bu
+         // classı newler ve onun üzerinden istekleri karşılar.
 public class ProductServiceImpl {
-    // Controllerın size akrıcağı işlemleri burada yaparsınız.
-    // iş kodu
+    private final List<Product> productsInMemory = new ArrayList<>();
+
+    public ProductCreatedResponse create(ProductForCreateDto productForCreateDto) { // bu parametreler
+        Product product = new Product();
+        product.setId(new Random().nextInt(100)); // Veritabanında id otomatik artar, biz burada random atıyoruz.
+        product.setName(productForCreateDto.getName());
+        product.setPrice(productForCreateDto.getPrice());
+
+        productsInMemory.add(product);
+
+        ProductCreatedResponse response = new ProductCreatedResponse(); // neden response oluşturduk? Çünkü controllerda
+                                                                        // ne dönüceğini belirtmemiz gerekiyor.
+                                                                        // Controllerda product dönebiliriz ama biz
+                                                                        // productCreatedResponse dönmek istiyoruz. O
+                                                                        // yüzden response oluşturduk.
+        response.setId(product.getId());
+        response.setName(product.getName());
+        response.setPrice(product.getPrice());
+
+        return response;
+
+        // Veritabanı işlemleri için repository kullanılır. Repository, veritabanı
+        // işlemlerini yapar.
+    }
 }
 
 // NOT: Controllerı service i çağırıcak ve service ne dönüyorsa onu return
